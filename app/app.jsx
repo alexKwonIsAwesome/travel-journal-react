38f56655ba73bdfbf3d29532011fdd844a403b17
alexKwonIsAwesome <khwalex@gmail.com>
2016-12-05 22:16:56 +0900

    init main App.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Provider} = require('react-redux');
 
 // Components
+var App = require('App');
 
 // Reducers
 
 // Store
-var store = createStore(rootReducer);
+var {createStore} = require('redux');
+var store = createStore(() => {});
 
 // Store Subscribe
 
@@ -18,7 +21,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <Provider store={store}>
-  </Provider>,
+  <App/>,
   document.getElementById('app')
 );
