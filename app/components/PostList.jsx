c09c3f4ccc6e9fb5f16dbc6aff08eb97a8144f95
alexKwonIsAwesome <khwalex@gmail.com>
2016-12-06 10:18:11 +0900

    update PostList: dummy object and loop

--- app/components/PostList.jsx
+++ app/components/PostList.jsx
@@ -1,16 +1,35 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var PostList = React.createClass({
+  renderPosts: function (posts) {
+    return posts.map((post) => {
+      return (
+        <li key={post.id}><Link to={"/post/" + post.id}>{post.title}</Link></li>
+      );
+    });
+  },
   render: function () {
-   return (
-     <ul>
-      <li>Intro</li>
-      <li>Prep</li>
-      <li>Day 1</li>
-      <li>Day 2</li>
-      <li>Day 3</li>
-     </ul>
-   );
+    var dummyPosts = [
+      {
+        id: 1,
+        title: 'intro',
+        content: 'love'
+      }, {
+        id: 2,
+        title: 'prep',
+        content: 'hey'
+      }, {
+        id: 3,
+        title: 'Day 1',
+        content: 'good day'
+      }
+    ];
+    return (
+      <ul>
+        {this.renderPosts(dummyPosts)}
+      </ul>
+    );
   }
 });
 
